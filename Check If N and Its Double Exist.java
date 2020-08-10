af2c8714a2fc12c8137946ed1cf1ecb7584e7868
Niladri Bhattacharjee <47671104+Niladri29@users.noreply.github.com>
2020-08-10 11:43:36 +0530

    Add files via upload

---
+++ Check If N and Its Double Exist.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]==arr[i]*2||arr[i]==arr[j]*2)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
